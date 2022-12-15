c53b169c4149503f1f5347c23401981fe0068839
zsj <939038111@qq.com>
2022-12-16 01:29:58 +0800

    finish lab 1

---
+++ src/main/mrcoordinator.go
@@ -0,0 +1,33 @@
+package main
+
+//
+// start the coordinator process, which is implemented
+// in ../mr/coordinator.go
+//
+// go run mrcoordinator.go pg*.txt
+//
+// Please do not change this file.
+//
+
+import (
+	"6.824/mr"
+	"log"
+)
+import "time"
+import "os"
+import "fmt"
+
+func main() {
+	log.SetFlags(log.Llongfile | log.Ltime)
+	if len(os.Args) < 2 {
+		fmt.Fprintf(os.Stderr, "Usage: mrcoordinator inputfiles...\n")
+		os.Exit(1)
+	}
+
+	m := mr.MakeCoordinator(os.Args[1:], 10)
+	for m.Done() == false {
+		time.Sleep(time.Second)
+	}
+
+	time.Sleep(time.Second)
+}
